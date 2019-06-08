81e262005a94166b38f2e189d4e901fe6bbc1d25
YaluzockerTV <45143027+YaluzockerTV@users.noreply.github.com>
2019-06-08 19:25:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 import discord
-from discord.ext import commands
-from discord.ext import Bot
-import asyncio
-import random
-import requests
-import os
+
 
 client = discord.Client()
-
-
-@client.event
-async def on_message(message):
-    if message.authoer == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.servermessage(message.channel, msg)
 
 @client.event
 async def on_ready():
     print('Logged in as')
     print('client.user.name')
-    print('chekc my website to start www.website.com')
+    print('This Bot is still in Alpha')
     print('------')
 
 client.run('NTg2NTc0MDA2NTM5Mzg2OTU1.XPqKCg.NPcGoo5ZoW4VE82GgF2z5EUcers')
